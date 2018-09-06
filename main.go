bbcacff34c3549fb43d50c094fc48607ee1de2f1
uenoryo <r.r.ag0611@outlook.jp>
2018-09-07 00:37:11 +0900

    dbに入るようにする.

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"log"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/uenoryo/ramen/ramen"
 )
 
 func main() {
+	mysql, err := db()
+	if err != nil {
+		log.Printf("error initialize database, %s", err.Error())
+		return
+	}
+	db := ramen.NewDatabase(mysql)
+	ramen := ramen.NewRamen(db)
 	if err := ramen.Init(); err != nil {
 		log.Printf("error ramen init, %s", err.Error())
 	}
+
+	if err := ramen.Set("test", "2012/10/10"); err != nil {
+		log.Printf("error ramen set, %s", err.Error())
+	}
+}
+
+func db() (*sql.DB, error) {
+	dbhost := os.Getenv("RAMEN_DB_HOST")
+	dbname := os.Getenv("RAMEN_DB_NAME")
+	user := os.Getenv("RAMEN_DB_USER")
+	password := os.Getenv("RAMEN_DB_PASSWORD")
+	if dbname == "" || user == "" {
+		return nil, errors.New("error connect database, database name and user name are required")
+	}
+	return sql.Open("mysql", fmt.Sprintf("%s:%s@%s/%s", user, password, dbhost, dbname))
 }
--- ramen/ramen.go
+++ ramen/ramen.go
@@ -4,23 +4,39 @@ import (
 	"database/sql"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strings"
 
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/pkg/errors"
 )
 
 const schemaPath = "./ramen.sql"
 
-func Init() error {
+type ramen struct {
+	storage Storage
+}
+
+type Storage interface {
+	init() error
+	save(datetime, memo string) error
+}
+
+type database struct {
+	dsn *sql.DB
+}
+
+func NewDatabase(dsn *sql.DB) *database {
+	return &database{
+		dsn: dsn,
+	}
+}
+
+func (db *database) init() error {
 	file, err := ioutil.ReadFile(schemaPath)
 	if err != nil {
 		return errors.Wrap(err, "error read sql file")
 	}
 	sql := strings.TrimSuffix(string(file), "\n")
 
-	db, err := db()
 	if err != nil {
 		return errors.Wrap(err, "error connect db")
 	}
@@ -30,20 +46,33 @@ func Init() error {
 		if q == "" {
 			continue
 		}
-		if _, err = db.Exec(q); err != nil {
+		if _, err = db.dsn.Exec(q); err != nil {
 			return errors.Wrapf(err, "error exec query, [%s]", q)
 		}
 	}
 	return nil
 }
 
-func db() (*sql.DB, error) {
-	dbhost := os.Getenv("RAMEN_DB_HOST")
-	dbname := os.Getenv("RAMEN_DB_NAME")
-	user := os.Getenv("RAMEN_DB_USER")
-	password := os.Getenv("RAMEN_DB_PASSWORD")
-	if dbname == "" || user == "" {
-		return nil, errors.New("error connect database, database name and user name are required")
+func (db *database) save(memo, datetime string) error {
+	q := fmt.Sprintf("INSERT INTO reminder(`memo`, `remember_at`) VALUES('%s', '%s')", memo, datetime)
+	_, err := db.dsn.Exec(q)
+	return err
+}
+
+func NewRamen(storage Storage) *ramen {
+	return &ramen{
+		storage: storage,
 	}
-	return sql.Open("mysql", fmt.Sprintf("%s:%s@%s/%s", user, password, dbhost, dbname))
+}
+
+func (ramen *ramen) Init() error {
+	return ramen.storage.init()
+}
+
+func (ramen *ramen) Set(memo, datetime string) error {
+	err := ramen.storage.save(memo, datetime)
+	if err != nil {
+		return errors.Wrap(err, "error save reminder")
+	}
+	return nil
 }
