d4423ab4e3f0bf34f61c2828aef5dcf64adb94e7
uenoryo <uenoryo@uma-b.com>
2019-05-24 22:32:50 +0900

    DBは使わない.

--- ramen.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `reminder`;
-CREATE TABLE `reminder` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `memo` varchar(255) NOT NULL,
-  `remember_at` DATETIME NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2554 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
