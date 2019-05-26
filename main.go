6c0571380e3ab7efd18b76c15f21cbd52cb91fd7
uenoryo <uenoryo@uma-b.com>
2019-05-26 15:52:01 +0900

    エラーハンドリングをちゃんとする.

--- main.go
+++ main.go
@@ -2,23 +2,35 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 
+	"github.com/pkg/errors"
 	"github.com/uenoryo/ramen/ramen"
 	yaml "gopkg.in/yaml.v2"
 )
 
+const (
+	configPath = "./config.yml"
+)
+
 func main() {
-	buf, err := ioutil.ReadFile("./config.yml")
+	if err := _main(); err != nil {
+		log.Fatal("Whoops!!", err.Error())
+	}
+}
+
+func _main() error {
+	buf, err := ioutil.ReadFile(configPath)
 	if err != nil {
-		panic(err)
+		return errors.Wrapf(err, "read config:%s failed", configPath)
 	}
 
 	cnf := ramen.Config{}
-	err = yaml.Unmarshal(buf, &cnf)
-	if err != nil {
-		panic(err)
+	if err := yaml.Unmarshal(buf, &cnf); err != nil {
+		return errors.Wrap(err, "yaml unmershal failed")
 	}
 
 	ramen := ramen.New(cnf)
 	ramen.Run()
+	return nil
 }
