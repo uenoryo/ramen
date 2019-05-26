4b52218361269712e522a54b0e22a95dbf0a54c8
uenoryo <uenoryo@uma-b.com>
2019-05-26 17:55:27 +0900

    add test.

--- ramen/ramen_test.go
+++ ramen/ramen_test.go
@@ -146,3 +146,99 @@ func Test_isBotName(t *testing.T) {
 		}
 	}
 }
+
+func Test_analysis(t *testing.T) {
+	t.Parallel()
+
+	type Expect struct {
+		To      string
+		Date    string
+		Time    string
+		Content string
+		Error   error
+	}
+
+	type Test struct {
+		Input  string
+		Expect Expect
+	}
+
+	cases := []Test{
+		{
+			Input: "@testbot 12/10 10:00 eat ramen",
+			Expect: Expect{
+				To:      "@testbot",
+				Date:    "12/10",
+				Time:    "10:00",
+				Content: "eat ramen",
+				Error:   nil,
+			},
+		},
+		{
+			Input: "@testbot 10:00 eat ramen",
+			Expect: Expect{
+				To:      "@testbot",
+				Date:    "",
+				Time:    "10:00",
+				Content: "eat ramen",
+				Error:   nil,
+			},
+		},
+		{
+			Input: "    @testbot    12/10     10:00     eat ramen   ",
+			Expect: Expect{
+				To:      "@testbot",
+				Date:    "12/10",
+				Time:    "10:00",
+				Content: "eat ramen",
+				Error:   nil,
+			},
+		},
+		{
+			Input: "testbot 12/10 10:00 eat ramen", // @ がない
+			Expect: Expect{
+				Error: ErrMissingBotBame,
+			},
+		},
+		{
+			Input: "@testbot 12/100 10:00 eat ramen", // 日付が変
+			Expect: Expect{
+				Error: ErrMissingRemindTime,
+			},
+		},
+		{
+			Input: "@testbot 12/10 :00 eat ramen", // 時間が変
+			Expect: Expect{
+				Error: ErrMissingRemindTime,
+			},
+		},
+	}
+
+	for _, test := range cases {
+		ramen := &Ramen{
+			client: &slack.Client{
+				BotName: "testbot",
+			},
+		}
+		to, date, time, content, err := ramen.analysis(test.Input)
+		if err != test.Expect.Error {
+			t.Fatalf("error is not match. got %v, want %v", err, test.Expect.Error)
+		}
+		if test.Expect.Error != nil {
+			continue
+		}
+
+		if to != test.Expect.To {
+			t.Errorf("error result to. got %s, want %s", to, test.Expect.To)
+		}
+		if date != test.Expect.Date {
+			t.Errorf("error result date. got %s, want %s", date, test.Expect.Date)
+		}
+		if time != test.Expect.Time {
+			t.Errorf("error result time. got %s, want %s", time, test.Expect.Time)
+		}
+		if content != test.Expect.Content {
+			t.Errorf("error result content. got %s, want %s", content, test.Expect.Content)
+		}
+	}
+}
